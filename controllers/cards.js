490e7d88bd7e36ee54ee2db0935846c56726de9d
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-13 12:02:32 +0700

    Sprint 13 | Errors handling corrections: controller/cards: All errors handler fixed (including statuses: 200, 201, 400, 404, 500).
    v1.0.3
    TODO: Errors handling.

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,7 +3,13 @@ const Card = require('../models/card');
 // Read ALL cards
 function getCards(req, res) {
   return Card.find({})
-    .then((cards) => res.status(200).send(cards))
+    .then((cards) => {
+      // Status 400:
+
+      // Status 200:
+      res.status(200).send(cards);
+    })
+    // Status 500 - Default
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}.` }));
 }
 
@@ -12,12 +18,19 @@ function createCard(req, res) {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner })
-    .then((card) => res.status(201).send(card))
+    .then((card) => {
+      // Status 400:
+
+      // Status 201:
+      res.status(201).send(card);
+    })
     .catch((err) => {
       if (err.name === 'ValidationError') {
+        // Status 400:
         res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
         return;
       }
+      // Status 500 - Default
       res.status(500).send({ message: `Произошла ошибка ${err}.` });
     });
 }
@@ -27,11 +40,18 @@ function deleteCard(req, res) {
   const { cardId } = req.params;
   return Card.findByIdAndDelete(cardId)
     .then((card) => {
+      // Status 404:
+      if (!card) {
+        res.status(404).send({ message: `Карточка с указанным _id: ${cardId} не найдена.` });
+        return;
+      }
+      // Status 200:
       res.status(200).send(card);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(404).send({ message: `Карточка с указанным id: ${cardId} не найдена.` });
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные для удаления карточки.' });
+        return;
       }
       res.status(500).send({ message: `Произошла ошибка ${err}.` });
     });
@@ -45,11 +65,22 @@ function likeCard(req, res) {
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-    .then((card) => res.status(201).send(card))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(404).send({ message: `Карточка с указанным id: ${cardId} не найдена.` });
+    .then((card) => {
+      // Status 404:
+      if (!card) {
+        res.status(404).send({ message: `Передан несуществующий _id: ${cardId} карточки` });
+        return;
       }
+      // Status 201:
+      res.status(201).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        // Status 400:
+        res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка.' });
+        return;
+      }
+      // Status 500 - Default
       res.status(500).send({ message: `Произошла ошибка ${err}.` });
     });
 }
@@ -62,11 +93,22 @@ function dislikeCard(req, res) {
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
-    .then((card) => res.status(201).send(card))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(404).send({ message: `Карточка с указанным id: ${cardId} не найдена.` });
+    .then((card) => {
+      // Status 404:
+      if (!card) {
+        res.status(404).send({ message: `Передан несуществующий _id: ${cardId} карточки` });
+        return;
       }
+      // Status 201:
+      res.status(201).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        // Status 400:
+        res.status(400).send({ message: 'Переданы некорректные данные для снятии лайка.' });
+        return;
+      }
+      // Status 500 - Default
       res.status(500).send({ message: `Произошла ошибка ${err}.` });
     });
 }
