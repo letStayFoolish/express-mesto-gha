643484a65e5d2c25f249463ed986c1af56ef5b18
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-25 20:36:22 +0200

    Sprint 14 | Authentication
    auth.js:
    - update checkAuthentication()
    correct: jwt
    remove: token
    
    add config.js
    
    v1.0.0

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,20 +3,19 @@ const BadRequest = require('../error_handlers/bad-request-400');
 
 const checkAuthentication = (req, res, next) => {
   const { jwt } = req.cookies;
-  const { token } = jwt;
-  if (!jwt) {
+  if (!req.cookies) {
     return next(new BadRequest('Доступ запрещен.'));
   }
   let payload;
   try {
-    payload = checkToken(token);
+    payload = checkToken(jwt);
   } catch (error) {
     return next(new BadRequest('Необходима авторизация.'));
   }
-  // req.user = payload;
-  req.user = {
-    _id: payload._id,
-  };
+  req.user = payload;
+  // req.user = {
+  //   _id: payload._id,
+  // };
   return next();
 };
 
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { URL_REGEX } = require('../utils/regex');
 const {
   login,
 } = require('../controllers/users');
