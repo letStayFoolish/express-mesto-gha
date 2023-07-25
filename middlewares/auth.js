9e127ba7817b32abb825e2d6b34b132add88ed76
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-07-25 22:27:47 +0200

    Sprint 14 | Authentication
    middleware/auth.js:
    - update checkAuthentication
    
    v1.0.0

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,7 @@ const RequestUnauthorized = require('../error_handlers/request-unauthorized-401'
 
 const checkAuthentication = (req, res, next) => {
   const { authorization } = req.cookies;
+
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new RequestUnauthorized('Необходима авторизация.'));
   }
